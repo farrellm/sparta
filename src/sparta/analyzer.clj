90bd9437a92690a97334fbdaa7fc60bb12470721
Matthew Farrell <farrellm@alum.mit.edu>
2014-12-22 02:05:39 -0500

    improve ns; add if

--- src/sparta/analyzer.clj
+++ src/sparta/analyzer.clj
@@ -20,7 +20,7 @@
 
 (defn *macroexpand-1 [form env]
   (match [form]
-    [(['ns n] :seq)] n
+    [(['ns n & _] :seq)] form
     :else (macroexpand-1 form)))
 
 (defn create-var [form env]
@@ -36,71 +36,91 @@
      (env/ensure (global-env)
                  (run-passes (-analyze form env))))))
 
+(def -emit-form nil)
 (defmulti -emit-form
-  (fn [ast]
+  (fn [ast set-ns]
     (if-let [type (:type ast)]
       type
       (if-let [op (:op ast)]
         op
         []))))
-(defmethod -emit-form :number [ast]
+(defmethod -emit-form :number [ast set-ns]
   (str (:val ast)))
-(defmethod -emit-form :string [ast]
+(defmethod -emit-form :string [ast set-ns]
   (str "'" (:val ast) "'"))
-(defmethod -emit-form :keyword [ast]
+(defmethod -emit-form :keyword [ast set-ns]
   (str "'" (:val ast) "'"))
-(defmethod -emit-form :maybe-class [ast]
+(defmethod -emit-form :maybe-class [ast set-ns]
   (str (:class ast)))
-(defmethod -emit-form :vector [ast]
-  (apply str "list(" (join ", " (mapcat -emit-form (:items ast))) ")"))
+(defmethod -emit-form :vector [ast set-ns]
+  (apply str "list(" (join ", " (mapcat #(-emit-form % set-ns) (:items ast))) ")"))
 
-(defmethod -emit-form :with-meta [ast]
-  (-> ast :expr -emit-form))
+(defmethod -emit-form :with-meta [ast set-ns]
+  (-> ast :expr (-emit-form set-ns)))
 
-(defmethod -emit-form :fn [ast]
+(defmethod -emit-form :fn [ast set-ns]
   (when-not (= 1 (-> ast :methods count))
     (throw (RuntimeException. "only functions of a single arity supported")))
 
-  (let [method (-> ast :methods first)]
-    (str "(function (" (join ", " (map :name (:params method))) ") "
-         (-> ast :methods first :body -emit-form)
-         ")")))
+  (let [method (-> ast :methods first)
+        f (str "function (" (join ", " (map :name (:params method))) ") "
+               (-> ast :methods first :body (-emit-form false)))]
+    (if set-ns
+      (str "{ f <- " f "; environment(f) <- .ns; f }"))))
 
-(defmethod -emit-form :local [ast]
+(defmethod -emit-form :local [ast set-ns]
   (str (:name ast)))
 
-(defmethod -emit-form :do [ast]
+(defmethod -emit-form :do [ast set-ns]
   (str "{ " (->> (-> ast :statements  (conj (-> ast :ret)))
-              (map -emit-form) (join "; ")) " }"))
+              (map #(-emit-form % set-ns)) (join "; ")) " }"))
 
-(defmethod -emit-form :let [ast]
+(defmethod -emit-form :let [ast set-ns]
   (str "{ "
-       (join "; " (concat (->> ast :bindings (map #(str (:name %) " <- " (-> % :init -emit-form))))
-                          [(-> ast :body -emit-form)]))
+       (join "; " (concat (->> ast :bindings
+                               (map #(str (:name %) " <- " (-> % :init (-emit-form set-ns)))))
+                          [(-> ast :body (-emit-form set-ns))]))
        " }"))
 
-(defmethod -emit-form :invoke [ast]
+(defmethod -emit-form :invoke [ast set-ns]
   (let [f (-> ast :fn)]
     (if-let [op (and (-> f :op (= :maybe-class)) (-> f )
                      (-> f :class (infix-ops)))]
-      (->> ast :args (map -emit-form) (join (str op)))
-      (str (-emit-form f)
-           "(" (->> ast :args (map -emit-form) (join ", ")) ")"))))
+      (->> ast :args (map #(-emit-form % set-ns)) (join (str op)))
+      (match f
+        {:class 'ns} #_(str "assign('.ns', as.environment('"
+                          (-> ast :args first :class)
+                          "'), envir = .GlobalEnv)")
+        (let [n (-> ast :args first :class)]
+          (str "{ if (!exists('" n "', envir=.GlobalEnv)) "
+               "assign('" n "', new.env(parent=emptyenv()), envir=.GlobalEnv); "
+               "assign('.ns', .GlobalEnv[['" n "']], envir=.GlobalEnv)"
+               " }"))
+        :else (str (-emit-form f set-ns)
+                   "(" (->> ast :args (map #(-emit-form % set-ns)) (join ", ")) ")")))))
 
-(defmethod -emit-form :def [ast]
-  (str (:var ast) " <<- " (-> ast :init -emit-form)))
+(defmethod -emit-form :def [ast set-ns]
+  (str "assign('" (-> ast :var str (.replaceAll "-" "."))
+       "', " (-> ast :init (-emit-form set-ns)) ", envir = .GlobalEnv$.ns)"))
 
-(defmethod -emit-form :host-call [ast]
+(defmethod -emit-form :host-call [ast set-ns]
   (if (= '- (-> ast :target :class))
     (str (:method ast) "("
-         (->> ast :args (map -emit-form) (join ", ")) ")")
+         (->> ast :args (map #(-emit-form % set-ns)) (join ", ")) ")")
     (str "as.environment('" (-> ast :target :class) "')$"
          (:method ast) "("
-         (->> ast :args (map -emit-form) (join ", ")) ")")))
+         (->> ast :args (map #(-emit-form % set-ns)) (join ", ")) ")")))
 
-(defmethod -emit-form :default [ast]
-  [ast])
+(defmethod -emit-form :if [ast set-ns]
+  (let [e (-> ast :else )]
+    (str "if (" (-> ast :test (-emit-form set-ns)) ") "
+         (-> ast :then (-emit-form set-ns))
+         (if-not (and (= (:op e):const) (= (:val e) nil))
+           (str " else " (-> ast :else (-emit-form set-ns)))))))
+
+(defmethod -emit-form :default [ast set-ns]
+  [ast set-ns])
 
 (defn emit-form [ast]
-  (str (-emit-form ast) (if (:top-level ast)
-                          "; " nil)))
+  (str (-emit-form ast (:top-level ast))
+       (if (:top-level ast) "; " nil)))
--- test/sparta/core_test.clj
+++ test/sparta/core_test.clj
@@ -12,8 +12,10 @@
     (is (= "':a'; " (emit-form (analyze ':a {}))))
     (is (= "list(a, b); " (emit-form (analyze '[a b] {}))))
     (is (= "{ 1; 2; 3 }; " (emit-form (analyze '(do 1 2 3) {}))))
-    (is (= "(function (x, y) { 1; 2; x }); " (emit-form (analyze '(fn [x y] 1 2 x) {}))))
-    (is (= "(function (x) { x }); " (emit-form (analyze '(fn [x] x) {}))))
+    (is (= "{ f <- function (x, y) { 1; 2; x }; environment(f) <- .ns; f }; "
+           (emit-form (analyze '(fn [x y] 1 2 x) {}))))
+    (is (= "{ f <- function (x) { x }; environment(f) <- .ns; f }; "
+           (emit-form (analyze '(fn [x] x) {}))))
     (is (= "{ x <- 0; { x } }; " (emit-form (analyze '(let [x 0] x) {}))))
     (is (= "{ x <- 1; y <- 8; { x+y } }; " (emit-form (analyze '(let [x 1 y 8] (+ x y)) {}))))
     (is (= "1+2; " (emit-form (analyze '(+ 1 2) {}))))
@@ -21,4 +23,8 @@
     (is (= "sqrt(8); " (emit-form (analyze '(.sqrt - 8) {}))))
     (is (= "as.environment('package:stats')$sqrt(8); "
            (emit-form (analyze '(.sqrt package:stats 8) {}))))
+    (is (= "{ if (!exists('core', envir=.GlobalEnv)) assign('core', new.env(parent=emptyenv()), envir=.GlobalEnv); assign('.ns', .GlobalEnv[['core']], envir=.GlobalEnv) }; "
+           (emit-form (analyze '(ns core) {}))))
+    (is (= "if (a) 8; " (emit-form (analyze '(if a 8) {}))))
+    (is (= "if (a) 8 else 0; " (emit-form (analyze '(if a 8 0) {}))))
     ))
